167eac4406748d9e88e9ff0ea221b567a03eb707
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-26 15:41:29 +0530

    gfgfg

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dist/",
+  base: "/ci-cd-using-github-action/",
   plugins: [react()],
 })
